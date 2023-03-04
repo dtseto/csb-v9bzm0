15b24fdd83ca0bef5dcdf08905b88c0d52f0efa7
Code Sandbox <hello@codesandbox.io>
2023-03-04 01:28:42 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.17fa447c51e463f59f72e3d4606662ea.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df86b4576a025298474045731053940e",
+    "url": "https://dtseto.github.io/csb-v9bzm0/index.html"
+  },
+  {
+    "revision": "e3e251d6598095d997f9",
+    "url": "https://dtseto.github.io/csb-v9bzm0/static/js/2.5c8e7925.chunk.js"
+  },
+  {
+    "revision": "491396a148cd61c88b92",
+    "url": "https://dtseto.github.io/csb-v9bzm0/static/js/main.992e4682.chunk.js"
+  },
+  {
+    "revision": "5e4d903f3a8b1892ea61",
+    "url": "https://dtseto.github.io/csb-v9bzm0/static/js/runtime-main.f926fc83.js"
+  }
+]);
